49916e33bce0955b8d79d38eabfecf900fa29181
s1601393 <s1601393@BC-5CG6253GLG.bc.local>
2017-09-21 14:10:01 +0300

    korttiin muutoksii

--- blackjackpeli/src/blackjackpeli/Blackjackpeli.java
+++ blackjackpeli/src/blackjackpeli/Blackjackpeli.java
@@ -15,7 +15,8 @@ public class Blackjackpeli {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic
+        shuffleArray(korttipakka);
+    
     }
     
 }
--- blackjackpeli/src/blackjackpeli/kortti.java
+++ blackjackpeli/src/blackjackpeli/kortti.java
@@ -1,9 +1,30 @@
 
 package blackjackpeli;
 
-class kortti {
+public class kortti {
     
-    
-    
-    
+   private int arvo;
+        private int maa;
+         
+        String[] maat = {"Hertta", "Ruutu", "Pata", "Risti"};
+        String[] arvot = {"Ässä", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+         
+        kortti(int maa,int arvo){
+             
+            this.maa=maa;
+            this.arvo=arvo;
+        }
+       
+        public @Override String toString(){
+            return arvot[arvo] + "," + maat[maa];
+         
+ 
+    }
+        public int getArvo(){
+            return arvo;
+        }
+        public int getMaa(){
+            return maa;
 }
+}
+ 
