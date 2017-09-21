04499e2af0e0a8e30526dd0bfad32dae1b504dbc
s1601383 <s1601383@BC-5CG6253LSY.bc.local>
2017-09-21 13:45:40 +0300

    lisätty kortti ja korttipakka

---
+++ blackjackpeli/src/blackjackpeli/kortti.java
@@ -0,0 +1,9 @@
+
+package blackjackpeli;
+
+class kortti {
+    
+    
+    
+    
+}
--- blackjackpeli/src/blackjackpeli/korttipakka.java
+++ blackjackpeli/src/blackjackpeli/korttipakka.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package blackjackpeli;
 
-/**
- *
- * @author s1601383
- */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import static javafx.scene.input.KeyCode.K;
+
+
 public class korttipakka {
+    public ArrayList<kortti> kortit = new ArrayList<kortti>();
+ 
+    
+    String[] arvot = {"2","3","4","5","6","7","8","9","10","jätkä","kuningatar","kuningas"};
+		String[] maat = {"pata","hertta","risti","ruutu"};
+		int[] korttienarvo = {2,3,4,5,6,7,8,9,10,10,10,10};
+		korttipakka k = new korttipakka(arvot, maat, korttienarvo);
+
+    private korttipakka(String[] arvot, String[] maat, int[] korttienarvo) {
+       
+    }
+
+                static void shuffleArray(int[] pakka) {
+        Random arpoja = new Random();
+        for (int i = pakka.length - 1; i > 0; i--) {
+            int index = arpoja.nextInt(i + 1);
+
+            int a = pakka[index];
+            pakka[index] = pakka[i];
+            pakka[i] = a;
+
+        }
+
+    }
+                
+
+    
+    
+    
     
 }
+    
+    
+    
+
