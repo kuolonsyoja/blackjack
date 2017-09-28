2c187f3e9322a5c5e12c046dbd63a7f3ccd0fbd3
s1601393 <s1601393@BC-5CG6253GLG.bc.local>
2017-09-28 12:58:34 +0300

    kortin evoluutio

--- blackjackpeli/src/blackjackpeli/kortti.java
+++ blackjackpeli/src/blackjackpeli/kortti.java
@@ -4,27 +4,32 @@ package blackjackpeli;
 public class kortti {
     
    private int arvo;
-        private int maa;
+        private String maa;
+    
          
-        String[] maat = {"Hertta", "Ruutu", "Pata", "Risti"};
-        String[] arvot = {"Ässä", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-         
-        kortti(int maa,int arvo){
-             
-            this.maa=maa;
-            this.arvo=arvo;
-        }
+        
        
-        public @Override String toString(){
-            return arvot[arvo] + "," + maat[maa];
-         
- 
-    }
-        public int getArvo(){
-            return arvo;
-        }
-        public int getMaa(){
-            return maa;
-}          
+@Override
+public String toString() {
+    return cardValue + " of " + suit;
+}
+
+
+public card(String suit, String cardValue) {
+    this.suit = suit;
+    this.cardValue = cardValue;
+}
+
+
+public card(String cardValue) {
+    this.cardValue = cardValue;
+}
+
+public String getSuit() {
+    return suit;
+}
+
+public void setSuit(String suit) {
+    this.suit = suit;
 }
  
