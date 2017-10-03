00c49a6713bf89e17d2b0d07d7e643beb0315472
s1601383 <s1601383@BC-5CG6253LSY.bc.local>
2017-10-03 10:55:49 +0300

    yes

--- blackjackpeli/src/blackjackpeli/Blackjackpeli.java
+++ blackjackpeli/src/blackjackpeli/Blackjackpeli.java
@@ -1,22 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package blackjackpeli;
 
-/**
- *
- * @author s1601374
- */
+import java.util.Scanner;
+
 public class Blackjackpeli {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-       
     
-    }
+        
+        public Blackjackpeli(){
+            Scanner lukija = new Scanner(System.in);
+     int pakkojenMaara = lukija.nextInt();
+      pakkojenmaara
+        }
+     
+    
+    
     
 }
