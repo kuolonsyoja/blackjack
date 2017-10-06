8e6272e715af9988616b0fab45323eaacd4cbe62
s1601374 <s1601374@BC-5CG6253L5P.bc.local>
2017-10-06 08:41:42 +0300

    suffle korjattu

--- blackjackpeli/src/blackjackpeli/kortti.java
+++ blackjackpeli/src/blackjackpeli/kortti.java
@@ -1,41 +1,41 @@
-package blackjackpeli;
+package kortit;
 
 public class kortti {
 
-    private int arvo;
-    private String maa;
+    private final int arvo;
+    private final int maa;
+    private boolean lukitus;
+    public final String[] maat = {"♥", "♦", "♠", "♣"};
+    public final String[] arvot = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
+    public kortti(int maa, int arvo) {
+        this.arvo = arvo;
+        this.maa = maa;
+        this.lukitus = false;
+    }
+
+    public int haeArvo() {
+        return this.arvo;
+    }
+
+    public int haeMaa() {
+        return this.maa;
+    }
+
+    public void lukitse() {
+        this.lukitus = true;
+    }
+
+    public void avaa() {
+        this.lukitus = false;
+    }
+    
+    public boolean olenkoLukittu() {
+        return this.lukitus;
+    }
+    
     @Override
     public String toString() {
-        return arvo + maa;
+        return this.maat[this.maa] + this.arvot[this.arvo];
     }
-
-    public kortti(String maa, int arvo) {
-        this.maa = maa;
-        this.arvo = arvo;
-    }
-
-    public String getSuit() {
-        return maa;
-    }
-
-    public void setSuit(String maa) {
-        this.maa = maa;
-    
-    }
-    
-     public int getArvo() {
-        return arvo;
-     }
-
-    public void setArvo(int arvo) {
-        this.arvo = arvo;
-    
-    }
-    
 }
-    
-    
-    
-    
-
